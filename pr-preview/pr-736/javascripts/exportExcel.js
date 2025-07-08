9d940ac1ffd9a9815cce4157c53b8d84b517f2b5
ravimeijerrig <ravimeijerrig@users.noreply.github.com>
2025-07-08 09:10:31 +0000

    Deploy preview for PR 736 🛫

--- pr-preview/pr-736/javascripts/exportExcel.js
+++ pr-preview/pr-736/javascripts/exportExcel.js
@@ -247,7 +247,7 @@ function setButtonState(button, disabled, text) {
     if (button) {
         button.disabled = disabled;
         if (disabled) {
-            button.innerHTML = '<i class="material-icons">hourglass_empty</i> ' + text;
+            button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style="width: 24px; height: 24px; vertical-align: middle; margin-right: 4px;"><path d="M6 2v6h.01L6 8.01 10 12l-4 4 .01.01H6V22h12v-5.99h-.01L18 16l-4-4 4-3.99-.01-.01H18V2H6z"/></svg> ' + text;
         }
     }
 }
